d0b5d9068e809b2a62bd6b38842267cbd4824155
doh46 <do@spsejecna.cz>
2025-04-19 12:37:08 +0200

    Repositioned everything to look neat

--- src/Login.java
+++ src/Login.java
@@ -25,33 +25,33 @@ public class Login extends JFrame implements ActionListener {
             Image image2  = cardIcon.getImage().getScaledInstance(120, 120, Image.SCALE_SMOOTH);
         ImageIcon icon2 = new ImageIcon(image2);
         JLabel cardLabel = new JLabel(icon2);
-        cardLabel.setBounds(670, 350, 120, 120);
+        cardLabel.setBounds(650, 100, 120, 120);
         add(cardLabel);
 
         //Welcome label
         welcomeLabel = new JLabel("Welcome to ATM");
-        welcomeLabel.setBounds(230, 125, 450, 40);
-        welcomeLabel.setFont(new Font("Tahoma", Font.BOLD, 12));
+        welcomeLabel.setBounds(315, 135, 450, 40);
+        welcomeLabel.setFont(new Font("Tahoma", Font.BOLD, 24));
         welcomeLabel.setForeground(Color.BLACK);
         add(welcomeLabel);
 
         //Card number label
         cardNumberLabel = new JLabel("Card Number: ");
-        cardNumberLabel.setBounds(150, 190, 375, 30);
+        cardNumberLabel.setBounds(200, 190, 375, 30);
         cardNumberLabel.setFont(new Font("Tahoma", Font.BOLD, 12));
         cardNumberLabel.setForeground(Color.BLACK);
         add(cardNumberLabel);
 
         //Card number
         cardNumber = new JTextField(15);
-        cardNumber.setBounds(325, 190, 230, 30);
+        cardNumber.setBounds(310, 190, 230, 30);
         cardNumber.setFont(new Font("Tahoma", Font.BOLD, 12));
         cardNumber.setForeground(Color.WHITE);
         add(cardNumber);
 
         //PIN label
         pinLabel = new JLabel("PIN: ");
-        pinLabel.setBounds(150, 250, 375, 30);
+        pinLabel.setBounds(200, 250, 375, 30);
         pinLabel.setFont(new Font("Tahoma", Font.BOLD, 12));
         pinLabel.setForeground(Color.BLACK);
         add(pinLabel);
@@ -59,22 +59,22 @@ public class Login extends JFrame implements ActionListener {
 
         //Password
         password = new JPasswordField(20);
-        password.setBounds(325, 250, 230, 30);
+        password.setBounds(310, 250, 230, 30);
         password.setFont(new Font("Tahoma", Font.BOLD, 12));
         password.setForeground(Color.WHITE);
         add(password);
 
         //Login button
         button1 = new JButton("Login");
-        button1.setBounds(300,300,300,30);
+        button1.setBounds(275,300,300,30);
         button1.setFont(new Font("Tahoma", Font.BOLD, 12));
         button1.setForeground(Color.BLACK);
         button1.addActionListener(this);
         add(button1);
 
         //Clear  button
-        button2 = new JButton("Login");
-        button2.setBounds(450,300,100,30);
+        button2 = new JButton("Clear");
+        button2.setBounds(275,400,300,30);
         button2.setFont(new Font("Tahoma", Font.BOLD, 12));
         button2.setForeground(Color.BLACK);
         button2.addActionListener(this);
@@ -82,7 +82,7 @@ public class Login extends JFrame implements ActionListener {
 
         //Sign up button
         button3 = new JButton("Sign up");
-        button3.setBounds(300, 350, 210, 30);
+        button3.setBounds(275, 350, 300, 30);
         button3.setFont(new Font("Tahoma", Font.BOLD, 12));
         button3.setForeground(Color.BLACK);
         button3.addActionListener(this);
