3be88de1f03ff4295522f74af6326bad9af7bf80
doh46 <do@spsejecna.cz>
2025-04-19 11:28:31 +0200

    Readded images, made bank and card icon into the Jframe.

--- src/Login.java
+++ src/Login.java
@@ -1,20 +1,38 @@
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.net.URL;
 
 public class Login extends JFrame implements ActionListener {
-    Login(){
-        JFrame frame = new JFrame("Login");
-        frame.setSize(500, 500);
-        frame.setLocationRelativeTo(null);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        JPanel panel = new JPanel();
-        panel.setLayout(null);
-        frame.add(panel);
+    JLabel label1, label2, label3;
+    JTextField text1;
+    JPasswordField password3;
+    JButton button1, button2, button3;
+
+    Login() {
+        //Bank icon
+        super("Login System");
+        ImageIcon bankIcon = new ImageIcon("src/Images/bank.png");
+            Image image = bankIcon.getImage().getScaledInstance(120, 120, Image.SCALE_SMOOTH);
+        ImageIcon icon = new ImageIcon(image);
+        JLabel bankLabel = new JLabel(icon);
+                bankLabel.setBounds(350, 10, 120, 120);
+        add(bankLabel);
+
+        //Card icon
+        ImageIcon cardIcon = new ImageIcon("src/Images/card.png");
+            Image image2  = cardIcon.getImage().getScaledInstance(120, 120, Image.SCALE_SMOOTH);
+        ImageIcon icon2 = new ImageIcon(image2);
+        JLabel cardLabel = new JLabel(icon2);
+        cardLabel.setBounds(670, 350, 120, 120);
+        add(cardLabel);
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
 
     }
+
+
 }
